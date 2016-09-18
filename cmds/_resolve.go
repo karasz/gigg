d024a91c3116350435c22961083457aee1d37d2e
Nagy Károly Gábriel <karasz@jpi.io>
2016-09-18 13:51:52 +0300

    gigg: added utility to extract search and nameservers from resolv.conf

---
+++ cmds/_resolve.go
@@ -0,0 +1,42 @@
+package cmds
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func parseresolv() ([]string, []string) {
+	var nameservers, search []string = make([]string, 0), make([]string, 0)
+
+	file, err := os.Open("/etc/resolv.conf")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, ";") || strings.HasPrefix(line, "#") {
+			continue
+		}
+		if strings.HasPrefix(line, "nameserver") {
+			nameservers = append(nameservers, strings.Fields(line)[1])
+		}
+		if strings.HasPrefix(line, "search") {
+			line = strings.TrimPrefix(line, "search")
+			for _, v := range strings.Fields(line) {
+				if v != "" {
+					search = append(search, v)
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	return nameservers, search
+}
